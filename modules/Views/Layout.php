8dd929a4ff8b848ed620eda0e712e2cc2a412629
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-10-08 10:13:10 +0200

    feat: test de vanilla framework, non definitif

--- modules/Views/Layout.php
+++ modules/Views/Layout.php
@@ -16,7 +16,7 @@ class Layout {
                 <meta charset="UTF-8">
                 <meta name="viewport" content="width=device-width, initial-scale=1">
                 <title><?php echo $title; ?></title>
-                <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+                <link rel="stylesheet" href="https://assets.ubuntu.com/v1/vanilla-framework-version-4.16.0.min.css" />
                 <link href="/_assets/styles/layout.css" rel="stylesheet">
                 <?php
                 if ($jsFilePath) {
@@ -24,21 +24,36 @@ class Layout {
                 }?>
             </head>
         <body>
-            <nav class="navbar navbar-expand-lg">
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="/homepage">ACCUEIL</a>
+        <header id="navigation" class="p-navigation">
+            <div class="p-navigation__row--25-75">
+                <div class="p-navigation__banner">
+                    <ul class="p-navigation__items">
+                        <li class="p-navigation__item">
+                            <button class="js-menu-button p-navigation__link">ACCUEIL</button>
                         </li>
-                        <li class="nav-item">
-                            <a class="nav-link active" href="/login">INTRAMU</a>
+                        <li class="p-navigation__item">
+                            <button class="js-menu-button p-navigation__link">INTRAMU</button>
                         </li>
                     </ul>
                 </div>
-            </nav>
+                <nav class="p-navigation__nav" aria-label="Example main">
+                    <ul class="p-navigation__items">
+                        <li class="p-navigation__item is-selected">
+                            <a class="p-navigation__link" href="#">ACCUEIL</a>
+                        </li>
+                        <li class="p-navigation__item">
+                            <a class="p-navigation__link" href="#">INTRAMU</a>
+                        </li>
+                    </ul>
+                    <ul class="p-navigation__items">
+                        <li class="p-navigation__item">
+                            <a class="p-navigation__link" href="#">A PROPOS</a>
+                        </li>
+
+                    </ul>
+                </nav>
+            </div>
+        </header>
         <?php
     }
 
@@ -48,7 +63,6 @@ class Layout {
      */
     public function renderBottom(): void {
         ?>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
         </body>
         </html>
         <?php
