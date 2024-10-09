a450f9e6e93ab22cedbe4ee773159459f8f82c29
KERBADOU-Islem-2326063b <islem.kerbadou@etu.univ-amu.fr>
2024-10-09 15:34:45 +0200

    feat: header/footer fini

--- _assets/scripts/layout.js
+++ _assets/scripts/layout.js
@@ -1,3 +1,6 @@
+/**
+ * Initialisation du Menu Hamburger(sidenav) Materialize
+ */
 document.addEventListener('DOMContentLoaded', function() {
     const elems = document.querySelectorAll('.sidenav');
     M.Sidenav.init(elems);
