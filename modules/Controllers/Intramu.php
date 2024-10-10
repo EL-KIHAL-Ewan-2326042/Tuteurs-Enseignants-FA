4dde1c49de552b7b8085c01a00b3d944cfecd20e
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-10-10 17:18:58 +0200

    feat: debut intranet

--- modules/Controllers/Intramu.php
+++ modules/Controllers/Intramu.php
@@ -2,20 +2,41 @@
 namespace Blog\Controllers;
 
 use Blog\Views\Layout;
+use Database;
 
+/**
+ * Contrôleur de la page de connexion
+ */
 class Intramu {
 
     /**
-     * Controlleur de la homepage
+     * Liaison entre la vue et le layout et affichage
+     * Gestion de la soumission du formulaire de connexion
      * @return void
      */
     public function show(): void {
-
-        $title = "Accueil";
-        $cssFilePath = '_assets/styles/intramu.css';
+        $title = "Connexion";
+        $cssFilePath = '_assets/styles/login.css';
         $jsFilePath = '';
+        $errorMessage = '';
 
-        $view = new \Blog\Views\Intramu();
+        if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['identifier']) && isset($_POST['password'])) {
+            $identifierLogs = $_POST['identifier'];
+            $passwordLogs = $_POST['password'];
+            $db = Database::getInstance();
+
+            $loginModel = new \Blog\Models\Intramu($db);
+
+            if ($loginModel->doLogsExist($identifierLogs, $passwordLogs)) {
+                $_SESSION['identifier'] = $identifierLogs;
+                header('Location: /homepage');
+                exit();
+            } else {
+                $errorMessage = 'Pseudo ou mot de passe incorrect';
+            }
+        }
+
+        $view = new \Blog\Views\Intramu($errorMessage);
 
         $layout = new Layout();
         $layout->renderTop($title, $cssFilePath);
