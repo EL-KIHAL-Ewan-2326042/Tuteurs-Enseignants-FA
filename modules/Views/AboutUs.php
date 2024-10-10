38014ef600f2e87797c38daae7e46f727bb1998c
Daphne <aviasdaphne@gmail.com>
2024-10-10 16:27:40 +0200

    page aboutus terminée

---
+++ modules/Views/AboutUs.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace Blog\Views;
+
+class AboutUs{
+    /**
+     * Vue de la page a propos
+     */
+
+    public function showView(){
+        ?>
+        <main>
+            <div class="row">
+                <div class="col s12 m7 l7 push-l5">
+                    <img class="responsive-img" src="https://imgur.com/AqAvrsS.png" alt="image de l'université">
+                </div>
+                <div class="col s12 m5 l5 pull-l7">
+                    <h3 id="title"> A Propos </h3> <br>
+                    <p id="text"> Ce site a été développé dans le cadre d'une Situation d'Apprentissage et d'Évaluation (SAE),
+                        avec pour objectif de faciliter la mise en relation entre les étudiants et les tuteurs. Conçu
+                        pour encourager l'entraide académique, notre plateforme permet aux étudiants de trouver rapidement
+                        un tuteur qui correspond à leurs besoins, selon les matières et les compétences recherchées.
+
+                        Le site propose plusieurs fonctionnalités essentielles, telles que l'inscription des tuteurs et
+                        des étudiants, un système de correspondance intelligent pour aider les étudiants à choisir le tuteur
+                        adéquat, ainsi que la possibilité de planifier des sessions de tutorat facilement.
+
+                        Notre équipe de développement, composée d'étudiants en informatique, s'est attelée à créer un outil
+                        fonctionnel et intuitif pour améliorer l'apprentissage collaboratif au sein de notre établissement.
+                        Chaque membre de l'équipe a contribué au projet avec ses compétences spécifiques en développement web,
+                        gestion de projet et UX design.
+
+                        Ce projet s'inscrit dans notre cursus académique et nous permet de mettre en pratique les compétences
+                        acquises au cours de nos études en informatique. En plus de l'aspect technique, il nous permet de
+                        comprendre les défis liés à la conception d'une application web dédiée à l'éducation.</p>
+                </div>
+            </div>
+        </main>
+        <?php
+    }
+}
