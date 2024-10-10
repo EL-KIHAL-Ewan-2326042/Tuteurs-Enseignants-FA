876fcd700b1a2bd226fad6d565ab77ded9f551e7
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-10-10 14:11:14 +0200

    chores: reglages bugs layout, debut intramu

---
+++ modules/Views/Intramu.php
@@ -0,0 +1,16 @@
+<?php
+namespace Blog\Views;
+
+class Intramu {
+    /**
+     * Vue de la homepage
+     * @return void
+     */
+    public function showView() {
+        ?>
+        <main>
+            <p>Ceci est un test</p>
+        </main>
+        <?php
+    }
+}
