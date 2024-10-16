1541e9b4d02887730063964a00215f3ab80ed5ff
Daphne <aviasdaphne@gmail.com>
2024-10-16 09:55:33 +0200

    IntramuTest.php OK

--- tests/Controllers/IntramuTest.php
+++ tests/Controllers/IntramuTest.php
@@ -15,12 +15,38 @@ use PHPUnit\Framework\TestCase;
  * fonctionne comme prévu
  */
 class IntramuTest extends TestCase{
+    /**
+     * Test de la méthode show() du contrôleur Intramu
+     * @return void
+     * @throws \PHPUnit\Framework\MockObject\Exception
+     */
     public function testShow(){
+        //initialisation session
+        $_SESSION['identifier'] = 'user';
+
         //Mock des classes layout et vue
         $mockLayout = $this->createMock(Layout::class);
         $mockView = $this->createMock(IntramuView::class);
 
-        //mockLayout
+        //attentes pour le mock du layout
+        $mockLayout->expects($this->once())
+            ->method('renderTop')
+            ->with($this->equalTo('Connexion'),$this->equalTo('_assets/styles/intramu.css'));
+
+        $mockLayout->expects($this->once())
+            ->method('renderBottom')
+            ->with($this->equalTo(''));
+
+        //attentes pour le mock de la vue
+        $mockView->expects($this->once())
+            ->method('showView');
+
+        //instanciation des mocks
+        $controller = new Intramu($mockLayout,$mockView);
+
+        //exécution
+        $controller->show();
+
     }
 
 }
