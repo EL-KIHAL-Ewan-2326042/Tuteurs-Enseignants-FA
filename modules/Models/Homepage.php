344da0c311db9e0a0698ba7f68d1bcf4f166b166
KERBADOU Islem <islem.kerbadou@etu.univ-amu.fr>
2024-10-17 11:10:21 +0200

    feat: ajout de plusieurs type recherche, et de joker pour la recherche

--- modules/Models/Homepage.php
+++ modules/Models/Homepage.php
@@ -14,27 +14,61 @@ class Homepage {
     public function correspondTerms(): array
     {
         $searchTerm = $_POST['search'] ?? '';
+        $searchType = $_POST['searchType'] ?? 'numeroEtudiant';
         $pdo = $this->db;
 
         $searchTerm = trim($searchTerm);
         $tsQuery = implode(' & ', explode(' ', $searchTerm));
 
-        $query = "
-        SELECT num_eleve, nom_eleve, prenom_eleve,
-        ts_rank_cd(to_tsvector('french', nom_eleve || ' ' || prenom_eleve), to_tsquery('french', :searchTerm), 32) AS rank
-        FROM eleve
-        WHERE to_tsquery('french', :searchTerm) @@ to_tsvector('french', nom_eleve || ' ' || prenom_eleve)
-        ORDER BY rank DESC
-        LIMIT 5
+        if ($searchType === 'numeroEtudiant') {
+            $query = "
+            SELECT num_eleve, nom_eleve, prenom_eleve
+            FROM eleve
+            WHERE num_eleve ILIKE :searchTerm
+            ORDER BY num_eleve
+            LIMIT 5
         ";
+            $searchTerm = "$searchTerm%";
+        } elseif ($searchType === 'nomDeFamille') {
+            $query = "
+            SELECT num_eleve, nom_eleve, prenom_eleve,
+            ts_rank_cd(to_tsvector('french', nom_eleve), to_tsquery('french', :searchTerm), 32) AS rank
+            FROM eleve
+            WHERE nom_eleve ILIKE :searchTerm
+            ORDER BY rank DESC
+            LIMIT 5
+        ";
+            $searchTerm = "$searchTerm%";
+        }
+        elseif ($searchType === 'prenom') {
+            $query = "
+            SELECT num_eleve, nom_eleve, prenom_eleve,
+            ts_rank_cd(to_tsvector('french', prenom_eleve), to_tsquery('french', :searchTerm), 32) AS rank
+            FROM eleve
+            WHERE prenom_eleve ILIKE :searchTerm
+            ORDER BY rank DESC
+            LIMIT 5
+        ";
+            $searchTerm = "$searchTerm%";
+        }
+        elseif ($searchType === 'nomEtPrenom') {
+            $query = "
+            SELECT num_eleve, nom_eleve, prenom_eleve,
+            ts_rank_cd(to_tsvector('french', nom_eleve || ' ' || prenom_eleve), to_tsquery('french', :searchTerm), 32) AS rank
+            FROM eleve
+            WHERE nom_eleve ILIKE :searchTerm OR prenom_eleve ILIKE :searchTerm
+            ORDER BY rank DESC
+            LIMIT 5
+            ";
+            $searchTerm = "%$searchTerm%";
+        }
 
         $stmt = $pdo->getConn()->prepare($query);
-        $stmt->bindValue(':searchTerm', $tsQuery);
+        $stmt->bindValue(':searchTerm', $searchTerm);
         $stmt->execute();
 
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
     public function getStudentAddress(string $studentId): string {
         if ($studentId !== $_POST['student_id']) {
             return false;
