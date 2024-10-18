12c78781ef1388cc0bb3f43ea36fef28d6760388
PELLET-Casimir-2326084 <144818615+PELLET-Casimir-2326084b@users.noreply.github.com>
2024-10-18 11:20:18 +0200

    Ajout d'une version de la V2 en anglais
    Mise en place pour respecter les conventions + ajout d'une documentation et du shéma de la base en MLD

---
+++ bd/v3/insertAddress_type.sql
@@ -0,0 +1,5 @@
+INSERT INTO Address_type (Type) VALUES ('Domicile_1');
+INSERT INTO Address_type (Type) VALUES ('Domicile_2');
+INSERT INTO Address_type (Type) VALUES ('Travail_1');
+INSERT INTO Address_type (Type) VALUES ('Travail_2');
+INSERT INTO Address_type (Type) VALUES ('Batiment');
