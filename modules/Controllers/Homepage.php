48d7c9c9e7bd45dd5ed819b9bdcb24ef06bc25bd
Daphne <aviasdaphne@gmail.com>
2024-10-21 08:54:53 +0200

    Correction contrôleur Homepage et la vue du Dashboard

--- modules/Controllers/Homepage.php
+++ modules/Controllers/Homepage.php
@@ -2,18 +2,19 @@
 namespace Blog\Controllers;
 
 use Blog\Views\Layout;
-use Blog\Views\Homepage as HomepageView;
 use Includes\Database;
+use Blog\Views\Homepage as HomepageView;
 
 class Homepage {
     private Layout $layout;
     private HomepageView $view;
+
     /**
      * Constructeur de la classe Homepage (controller)
      * @param Layout $layout Instance de la classe Layout
      * @param HomepageView $view Instance de la classe HomepageView
      */
-    public function __construct(Layout $layout,HomepageView $view) {
+    public function __construct(Layout $layout, HomepageView $view) {
         $this->layout = $layout;
         $this->view = $view;
     }
@@ -64,7 +65,7 @@ class Homepage {
         $jsFilePath = '/_assets/scripts/homepage.js';
 
         $this->layout->renderTop($title, $cssFilePath);
-        $this->view->showView($_SESSION['selected_student']['address']);
+        $this->view->showView();
         $this->layout->renderBottom($jsFilePath);
     }
 }
