17f7e90c1187ca5770ffa9e36ece58c43b9645a0
Daphne <aviasdaphne@gmail.com>
2024-10-25 10:41:56 +0200

    Error404Test.php (vue) OK

---
+++ tests/Views/Error404Test.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Views;
+
+use Blog\Views\Error404;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Classe de Error404Test
+ *
+ * Test de la vue Error404 : s'assure que la méthode showView()
+ * fonctionne comme prévu
+ */
+class Error404Test extends TestCase {
+    public function testError404() {
+        //instance de la classe
+        $error404 = new Error404();
+
+        //obtention du contenu généré
+        ob_start();
+        $error404->showView();
+        $output = ob_get_clean();
+
+        //vérification de l'affichage du contenu
+        $this->assertStringContainsString('Oups! Erreur 404', $output);
+        $this->assertStringContainsString('Désolé, la page que vous recherchez est introuvable.', $output);
+        $this->assertStringContainsString('Retour à la page d\'accueil', $output);
+        $this->assertStringContainsString('<a href="/" class="btn waves-effect waves-light">', $output);
+    }
+}
