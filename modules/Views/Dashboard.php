cb53e975fafd24c4b183f27060a8c2e8208c38ab
Daphne <aviasdaphne@gmail.com>
2024-12-12 11:45:41 +0100

    Réglage de l'importation de données sur la Dashboard à tester et affichage du Footer

--- modules/Views/Dashboard.php
+++ modules/Views/Dashboard.php
@@ -13,32 +13,15 @@ class Dashboard{
             <h3> Dashboard </h3>
             <div class="card-panel white">
                 <h4>Importer :</h4>
-<!--                <div class="row">-->
-<!--                    <h5> Rajouter des étudiants : </h5>-->
-<!--                    <form action="/dashboard" method="POST" enctype="multipart/form-data">-->
-<!--                        <div class="file-field input-field">-->
-<!--                            <div class="btn">-->
-<!--                                <span>Fichier CSV</span>-->
-<!--                                <input type="file" name="csv_file_student" id="csv_file_student" accept=".csv" multiple>-->
-<!--                            </div>-->
-<!--                            <div class="file-path-wrapper">-->
-<!--                                <input class="file-path validate" type="text" placeholder="Choisissez un fichier CSV" required>-->
-<!--                            </div>-->
-<!--                            <button class="btn waves-effect waves-light" type="submit" name="submit_student">Valider-->
-<!--                                <i class="material-icons right">send</i>-->
-<!--                            </button>-->
-<!--                        </div>-->
-<!--                    </form>-->
-<!--                </div>-->
 
                 <div class="row">
                     <h5> Rajouter des étudiants : </h5>
                     <form action="/dashboard" method="POST" enctype="multipart/form-data">
-                        <input type="hidden" name="table_name" value="student"> <!-- Champ caché pour spécifier la table -->
+                        <input type="hidden" name="table_name" value="student">
                         <div class="file-field input-field">
-                            <div class="btn">
+                            <div class="btn btn-behind">
                                 <span>Fichier CSV</span>
-                                <input type="file" name="csv_file_student" id="csv_file_student" accept=".csv" multiple>
+                                <input type="file" name="student" id="student" accept=".csv" multiple>
                             </div>
                             <div class="file-path-wrapper">
                                 <input class="file-path validate" type="text" placeholder="Choisissez un fichier CSV" required>
@@ -53,10 +36,11 @@ class Dashboard{
                 <div class="row">
                     <h5> Rajouter des professeurs : </h5>
                     <form action="/dashboard" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="table_name" value="teacher">
                         <div class="file-field input-field">
-                            <div class="btn">
+                            <div class="btn btn-behind">
                                 <span>Fichier CSV</span>
-                                <input type="file" name="csv_file_teacher" id="csv_file_teacher" accept=".csv" multiple>
+                                <input type="file" name="teacher" id="teacher" accept=".csv" multiple>
                             </div>
                             <div class="file-path-wrapper">
                                 <input class="file-path validate" type="text" placeholder="Choisissez un fichier CSV" required>
@@ -69,12 +53,13 @@ class Dashboard{
                 </div>
 
                 <div class="row">
-                    <h5> Rajouter des entreprises : </h5>
+                    <h5> Rajouter des stages : </h5>
                     <form action="/dashboard" method="POST" enctype="multipart/form-data">
+                        <input type="hidden" name="table_name" value="internship">
                         <div class="file-field input-field">
-                            <div class="btn">
+                            <div class="btn btn-behind">
                                 <span>Fichier CSV</span>
-                                <input type="file" name="csv_file_internship" id="csv_file_internship" accept=".csv" multiple>
+                                <input type="file" name="internship" id="internship" accept=".csv" multiple>
                             </div>
                             <div class="file-path-wrapper">
                                 <input class="file-path validate" type="text" placeholder="Choisissez un fichier CSV" required>
@@ -95,12 +80,12 @@ class Dashboard{
                             <option value="" disabled selected>Choisissez la liste à exporter</option>
                             <option value="student">Etudiants</option>
                             <option value="teacher">Professeurs</option>
-                            <option value="internship">Entreprises</option>
+                            <option value="internship">Stages</option>
                         </select>
                         <label>Choisissez la liste à exporter</label>
                     </div>
                     <div class="input-field">
-                        <button class="btn waves-effect waves-light" type="submit">Exporter
+                        <button class="btn waves-effect waves-light btn-behind" type="submit">Exporter
                             <i class="material-icons right">send</i>
                         </button>
                     </div>
