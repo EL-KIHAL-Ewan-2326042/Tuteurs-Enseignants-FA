9bfd50682d4d559ebed2306890f38750ce4d9ab4
Titouan <titouan.alvares@etu-univ-amu.fr>
2025-01-08 16:48:04 +0100

    chores: linter controller MentionsLegales

--- modules/Controllers/Mentionslegales.php
+++ modules/Controllers/Mentionslegales.php
@@ -1,35 +1,71 @@
 <?php
+/**
+ * Fichier contenant le contrôleur de la page 'Mentions légales'
+ *
+ * PHP version 8.3
+ *
+ * @category View
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
 
 namespace Blog\Controllers;
 
 use Blog\Views\layout\Layout;
 
 /**
- * Contrôleur de la page mentions légales
+ * Classe gérant les échanges de données entre
+ * le modèle et la vue de la page 'Mentions légales'
+ *
+ * PHP version 8.3
+ *
+ * @category View
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
  */
-class Mentionslegales{
-    private Layout $layout;
+class Mentionslegales
+{
+    private Layout $_layout;
 
     /**
-     * Constructeur de la classe Mentionslegales (contrôleur)
+     * Initialise les attributs passés en paramètre
+     *
      * @param Layout $layout Instance de la classe Layout
+     *                       servant de vue pour la mise en page
      */
-    public function __construct(Layout $layout){
-        $this->layout = $layout;
+    public function __construct(Layout $layout)
+    {
+        $this->_layout = $layout;
     }
 
     /**
      * Controlleur de la page mentions légales
+     *
      * @return void
      */
-    public function show(): void {
+    public function show(): void
+    {
         $title = "Mentions légales";
         $cssFilePath = '_assets/styles/mentionLeg.css';
         $jsFilePath = '';
         $view = new \Blog\Views\mentions_legales\Mentionslegales();
 
-        $this->layout->renderTop($title, $cssFilePath);
+        $this->_layout->renderTop($title, $cssFilePath);
         $view->showView();
-        $this->layout->renderBottom($jsFilePath);
+        $this->_layout->renderBottom($jsFilePath);
     }
 }
