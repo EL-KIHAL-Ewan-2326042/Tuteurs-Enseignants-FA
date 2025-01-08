a05a35ee4836efeb9162cf58088000ee28b4004d
Titouan <titouan.alvares@etu-univ-amu.fr>
2025-01-08 17:01:39 +0100

    chores: linter controller Error404

--- modules/Controllers/Error404.php
+++ modules/Controllers/Error404.php
@@ -1,24 +1,61 @@
 <?php
-
+/**
+ * Fichier contenant le contrôleur de la page 'Erreur 404'
+ *
+ * PHP version 8.3
+ *
+ * @category Controller
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
 
 namespace Blog\Controllers;
 
 use Blog\Views\layout\Layout;
 
-class Error404 {
+/**
+ * Classe gérant les échanges de données entre
+ * le modèle et la vue de la page 'Erreur 404'
+ *
+ * PHP version 8.3
+ *
+ * @category Controller
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
+class Error404
+{
 
-    private Layout $layout;
+    private Layout $_layout;
 
     /**
-     * Constructeur de la classe Error404 (controller)
+     * Initialise les attributs passés en paramètre
+     *
      * @param Layout $layout Instance de la classe Layout
+     *                       servant de vue pour la mise en page
      */
-    public function __construct(Layout $layout) {
-        $this->layout = $layout;
+    public function __construct(Layout $layout)
+    {
+        $this->_layout = $layout;
     }
 
     /**
-     * Controlleur de la page Erreur 404
+     * Contrôleur de la page 'Erreur 404'
+     *
      * @return void
      */
     public function show(): void
@@ -28,8 +65,8 @@ class Error404 {
         $jsFilePath = '';
         $view = new \Blog\Views\errors\Error404();
 
-        $this->layout->renderTop($title, $cssFilePath);
+        $this->_layout->renderTop($title, $cssFilePath);
         $view->showView();
-        $this->layout->renderBottom($jsFilePath);
+        $this->_layout->renderBottom($jsFilePath);
     }
 }
