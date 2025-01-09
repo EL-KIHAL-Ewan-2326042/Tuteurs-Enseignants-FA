1ad3fb9083ad4900584ebd3732f4d382fa396071
Titouan <titouan.alvares@etu-univ-amu.fr>
2025-01-09 11:38:48 +0100

    chores: linter model Teacher + annulation des dernières modifications

--- modules/Models/Teacher.php
+++ modules/Models/Teacher.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Fichier contenant le modèle associé aux enseignants
+ *
+ * PHP version 8.3
+ *
+ * @category Model
+ * @package  TutorMap/modules/Models
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
 
 namespace Blog\Models;
 
@@ -6,23 +22,57 @@ use Includes\Database;
 use PDO;
 use PDOException;
 
-class Teacher extends Model {
-    private Database $db;
-    public function __construct(Database $db) {
+/**
+ * Classe gérant toutes les fonctionnalités du site associées
+ * aux enseignants. Elle hérite de la classe 'Model'
+ *
+ * PHP version 8.3
+ *
+ * @category Model
+ * @package  TutorMap/modules/Models
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
+class Teacher extends Model
+{
+    private Database $_db;
+
+    /**
+     * Initialise les attributs passés en paramètre
+     *
+     * @param Database $db Instance de la classe Database
+     *                     servant de lien avec la base de données
+     */
+    public function __construct(Database $db)
+    {
         parent::__construct($db);
-        $this->db = $db;
+        $this->_db = $db;
     }
 
     /**
-     * @param string $identifier
-     * @return array|null
+     * Récupère le nom et le prénom de l'enseignant passé en paramètre
+     *
+     * @param string $identifier Identifiant de l'enseignant
+     *
+     * @return array|null Renvoie null si l'identifiant est vide,
+     * sinon renvoie une liste contenant le nom et le prénom de
+     * l'enseignant s'il les trouve, false sinon
      */
-    public function getFullName(string $identifier): ?array {
+    public function getFullName(string $identifier): ?array
+    {
         if (empty($identifier)) {
             return null;
         }
-        $db = $this->db;
-        $query = 'SELECT teacher_name, teacher_firstname FROM teacher WHERE id_teacher = :id_teacher';
+        $db = $this->_db;
+        $query = 'SELECT teacher_name, teacher_firstname
+                    FROM teacher
+                    WHERE id_teacher = :id_teacher';
         $stmt = $db->getConn()->prepare($query);
         $stmt->bindParam(':id_teacher', $identifier);
         $stmt->execute();
@@ -30,48 +80,59 @@ class Teacher extends Model {
     }
 
     /**
-     * Recuperer toute une ligne selon la cle primaire dans la table teacher
-     * @param string $Id_teacher
-     * @return false|mixed renvoie la ligne dans la DB
+     * Récupère les adresses de l'enseignant passé en paramètre
+     *
+     * @param string $id_teacher Identifiant de l'enseignant
+     *
+     * @return false|array Renvoie une liste contenant les adresses de l'enseignant,
+     * false sinon
      */
-    public function getAddress(string $Id_teacher): false|array {
+    public function getAddress(string $id_teacher): false|array
+    {
 
-        $db = $this->db;
+        $db = $this->_db;
         $query = 'SELECT address FROM has_address WHERE id_teacher = :id_teacher';
         $stmt = $db->getConn()->prepare($query);
-        $stmt->bindParam(':id_teacher', $Id_teacher);
+        $stmt->bindParam(':id_teacher', $id_teacher);
         $stmt->execute();
 
         return $stmt->fetchAll($db->getConn()::FETCH_ASSOC);
     }
 
     /**
-     * Récupère une liste des identifiants des enseignants associés aux départements du rôle de l'admin.
+     * Récupère une liste des identifiants des enseignants
+     * associés aux départements du rôle de l'admin
      *
-     * @return array|false Tableau contenant les identifiants des enseignants ou `false` en cas d'erreur si aucun enseignant n'est trouvé pour les départements spécifiés.
+     * @return array|false Tableau contenant les identifiants des enseignants,
+     * ou `false` en cas d'erreur si aucun enseignant n'est trouvé
+     * pour les départements spécifiés.
      */
-    public function createListTeacher() {
+    public function createListTeacher(): false|array
+    {
         $roleDepartments = $_SESSION['role_department'];
         $placeholders = implode(',', array_fill(0, count($roleDepartments), '?'));
 
-        $query = "SELECT Teacher.Id_teacher FROM Teacher JOIN Has_role ON Teacher.Id_Teacher = Has_role.User_id
+        $query = "SELECT Teacher.Id_teacher
+                    FROM Teacher
+                    JOIN Has_role ON Teacher.Id_Teacher = Has_role.User_id
                     where Department_name IN ($placeholders)";
 
-        $stmt = $this->db->getConn()->prepare($query);
+        $stmt = $this->_db->getConn()->prepare($query);
         $stmt->execute($roleDepartments);
         return $stmt->fetchAll(PDO::FETCH_COLUMN);
     }
 
     /**
-     * Recherche des termes correspondants dans la base de données en fonction des paramètres fournis dans le POST.
+     * Recherche des termes correspondants dans la base de données
+     * en fonction des paramètres fournis dans le POST.
      *
-     * @return array -Tableau associatif contenant les résultats de la recherche.
+     * @return array|false Renvoie un tableau associatif contenant
+     * les résultats de la recherche, false sinon
      */
-
-    public function correspondTermsTeacher(): array
+    public function correspondTermsTeacher(): array|false
     {
         $searchTerm = $_POST['search'] ?? '';
-        $pdo = $this->db;
+        $pdo = $this->_db;
 
         $searchTerm = trim($searchTerm);
 
@@ -93,31 +154,44 @@ class Teacher extends Model {
     }
 
     /**
-     * Récupère le nombre maximum de stagiaires et alternants que l'enseignant passé en paramètre peut avoir
-     * @param string $teacher numéro de l'enseignant
-     * @return false|string nombre maximum de stagiaires et alternants, sinon false
+     * Récupère le nombre maximum de stagiaires et alternants
+     * que l'enseignant passé en paramètre peut avoir
+     *
+     * @param string $teacher Identifiant de l'enseignant
+     *
+     * @return false|string Renvoie le nombre maximum de stagiaires et alternants,
+     * false sinon
      */
-    public function getMaxNumberInterns(string $teacher): false|string {
+    public function getMaxNumberInterns(string $teacher): false|string
+    {
         $query = 'SELECT maxi_number_trainees
                     FROM teacher
                     WHERE id_teacher = :teacher';
-        $stmt = $this->db->getConn()->prepare($query);
+        $stmt = $this->_db->getConn()->prepare($query);
         $stmt->bindParam(':teacher', $teacher);
         $stmt->execute();
         return $stmt->fetch(PDO::FETCH_COLUMN);
     }
 
     /**
-     * Met à jour le nombre maximum de stagiaires et alternants que l'enseignant passé en paramètre peut avoir
-     * @param string $teacher numéro de l'enseignant
-     * @param int $maxi_number_trainees nouveau nombre maximum de stagiaires et alternants
-     * @return true|string renvoie true si l'update a fonctionné, sinon l'erreur dans un string
+     * Met à jour le nombre maximum de stagiaires et alternants
+     * que l'enseignant passé en paramètre peut avoir
+     *
+     * @param string $teacher              numéro de
+     *                                     l'enseignant
+     * @param int    $maxi_number_trainees nouveau nombre maximum
+     *                                     de stagiaires et alternants
+     *
+     * @return true|string renvoie true si l'update a fonctionné,
+     * sinon une chaîne de caractères contenant l'erreur
      */
-    public function updateMaxiNumberTrainees(string $teacher, int $maxi_number_trainees): true|string {
+    public function updateMaxiNumberTrainees(
+        string $teacher, int $maxi_number_trainees
+    ): true|string {
         $query = 'UPDATE teacher
                     SET maxi_number_trainees = :maxi_number_trainees
                     WHERE id_teacher = :teacher';
-        $stmt = $this->db->getConn()->prepare($query);
+        $stmt = $this->_db->getConn()->prepare($query);
         $stmt->bindParam(':maxi_number_trainees', $maxi_number_trainees);
         $stmt->bindParam(':teacher', $teacher);
 
@@ -131,34 +205,58 @@ class Teacher extends Model {
 
 
     /**
-     * Renvoie le ou les disiplines d'un professeur
-     * @param string $id_teahcer identifiant du prof
+     * Renvoie les disiplines qu'enseigne le professeur passé en paramètre
+     *
+     * @param string $id_teacher Identifiant du prof
+     *
      * @return array|false result de la requete
      */
-    public function getDisciplines(string $id_teahcer) {
-        $pdo = $this->db;
+    public function getDisciplines(string $id_teacher): false|array
+    {
+        $pdo = $this->_db;
 
         $query = "SELECT discipline_name FROM is_taught WHERE id_teacher = :id";
         $stmt = $pdo->getConn()->prepare($query);
-        $stmt->bindParam(':id', $id_teahcer);
+        $stmt->bindParam(':id', $id_teacher);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_COLUMN);
     }
 
     /**
-     * Renvoie un tableau contenant tous les stages à venir des étudiants faisant partie des départements passés en paramètre et n'ayant pas encore de tuteur, et leurs informations
-     * Les stages sélectionnés sont uniquement ceux des élèves faisant partie d'au moins un des départements passés en paramètre
+     * Renvoie un tableau contenant tous les stages à venir des étudiants faisant
+     * partie des départements passés en paramètre et n'ayant pas encore de tuteur,
+     * et leurs informations. Les stages sélectionnés sont uniquement ceux des
+     * étudiants faisant partie d'au moins un des départements passés en paramètre.
      * Les stages n'ont pas encore débuté et n'ont aucun tuteur attribué
-     * @param array $departments liste des départements dont on veut récupérer les stages des élèves
-     * @param string $identifier identifiant de l'enseignant
-     * @return array tableau contenant les informations relatives à chaque stage, le nombre fois où l'enseignant connecté a été le tuteur de l'élève ainsi qu'une note représentant la pertinence du stage pour l'enseignant
+     *
+     * @param array      $departments     Liste des départements dont on
+     *                                    veut récupérer les stages des
+     *                                    étudiants
+     * @param string     $identifier      Identifiant de l'enseignant
+     * @param Internship $internshipModel Instance de la classe Internship
+     *                                    servant de modèle
+     * @param Department $departmentModel Instance de la classe Department
+     *                                    servant de modèle
+     *
+     * @return array Renvoie un tableau contenant les informations relatives à chaque
+     * stage, le nombre fois où l'enseignant connecté a été le tuteur de l'étudiant
+     * ainsi qu'une note représentant la pertinence du stage pour l'enseignant
      */
-    public function getStudentsList(array $departments, string $identifier, Internship $internshipModel, Department $departmentModel): array {
-        // on récupère pour chaque élève des départements de $departments les informations de leur prochain stage s'ils ont en un et s'ils n'ont pas encore de tuteur
+    public function getStudentsList(
+        array $departments,
+        string $identifier,
+        Internship $internshipModel,
+        Department $departmentModel
+    ): array {
+        // on récupère pour chaque étudiant des départements de $departments
+        // les informations de leur prochain stage s'ils ont en un et
+        // s'ils n'ont pas encore de tuteur
         $studentsList = array();
-        foreach($departments as $department) {
+        foreach ($departments as $department) {
             $newList = $departmentModel->getInternshipsPerDepartment($department);
-            if($newList) $studentsList = array_merge($studentsList, $newList);
+            if ($newList) {
+                $studentsList = array_merge($studentsList, $newList);
+            }
         }
 
         // on supprime les doubles s'il y en a
@@ -166,42 +264,53 @@ class Teacher extends Model {
 
         // on stocke les stages déjà demandés par l'enseignant
         $requests = $internshipModel->getRequests($identifier);
-        if(!$requests) $requests = array();
+        if (!$requests) {
+            $requests = array();
+        }
 
-        // pour chaque stage on initialise de nouveaux attributs qui leur sont relatifs
-        foreach($studentsList as &$row) {
+        // pour chaque stage on initialise
+        // de nouveaux attributs qui leur sont relatifs
+        foreach ($studentsList as &$row) {
             // le nombre de stages complétés par l'étudiant
             $internships = $internshipModel->getInternships($row['student_number']);
 
-            // l'année durant laquelle le dernier stage/alternance de l'étudiant a eu lieu avec l'enseignant comme tuteur
+            // l'année durant laquelle le dernier stage/alternance
+            // de l'étudiant a eu lieu avec l'enseignant comme tuteur
             $row['year'] = "";
 
             // le nombre de fois où l'enseignant a été le tuteur de l'étudiant
-            $row['internshipTeacher'] = $internships ? $internshipModel->getInternshipTeacher($internships, $identifier, $row['year']) : 0;
+            $row['internshipTeacher'] = $internships ? $internshipModel
+                ->getInternshipTeacher($internships, $identifier, $row['year']) : 0;
 
             // true si l'enseignant a déjà demandé à tutorer le stage, false sinon
             $row['requested'] = in_array($row['internship_identifier'], $requests);
 
-            // durée en minute séparant l'enseignant de l'adresse de l'entreprise où l'étudiant effectue son stage
-            $row['duration'] = $internshipModel->getDistance($row['internship_identifier'], $identifier, isset($row['id_teacher']));
+            // durée en minute séparant l'enseignant de l'adresse
+            // de l'entreprise où l'étudiant effectue son stage
+            $row['duration'] = $internshipModel->getDistance(
+                $row['internship_identifier'], $identifier, isset($row['id_teacher'])
+            );
         }
 
         return $studentsList;
     }
 
     /**
-     * Renvoie tous les départements de l'enseignant passé en paramètre
-     * @param string $teacher_id identifiant de l'enseignant
-     * @return false|array tableau contenant tous les départements dont l'enseignant connecté fait partie, false sinon
+     * Récupère tous les départements où enseigne le professeur passé en paramètre
+     *
+     * @param string $teacher_id Identifiant de benignant
+     *
+     * @return false|array Renvoie un tableau contenant tous les départements dont
+     * l'enseignant connecté fait partie, false sinon
      */
-    public function getDepTeacher(string $teacher_id): false|array {
+    public function getDepTeacher(string $teacher_id): false|array
+    {
         $query = 'SELECT DISTINCT department_name
                     FROM has_role
                     WHERE user_id = :teacher_id';
-        $stmt = $this->db->getConn()->prepare($query);
+        $stmt = $this->_db->getConn()->prepare($query);
         $stmt->bindParam(':teacher_id', $teacher_id);
         $stmt->execute();
         return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-
 }
