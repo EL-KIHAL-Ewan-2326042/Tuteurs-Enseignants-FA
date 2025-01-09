453809dc15f385de1629c0b75ffdbeb29a9fdac6
Titouan <titouan.alvares@etu-univ-amu.fr>
2025-01-09 09:25:22 +0100

    chores: linter controller Account

--- modules/Controllers/Account.php
+++ modules/Controllers/Account.php
@@ -1,4 +1,20 @@
 <?php
+/**
+ * Fichier contenant le contrôleur de la page 'Compte'
+ *
+ * PHP version 8.3
+ *
+ * @category Controller
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
 
 namespace Blog\Controllers;
 
@@ -8,23 +24,45 @@ use Blog\Views\account\Account as AccountView;
 use Blog\Views\layout\Layout;
 use Includes\Database;
 
-class Account {
-    private Layout $layout;
+/**
+ * Classe gérant les échanges de données entre
+ * le modèle et la vue de la page 'Compte'
+ *
+ * PHP version 8.3
+ *
+ * @category Controller
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
+class Account
+{
+    private Layout $_layout;
 
     /**
-     * Constructeur de la classe Account (controller)
+     * Initialise les attributs passés en paramètre
+     *
      * @param Layout $layout Instance de la classe Layout
+     *                       servant de vue pour la mise en page
      */
-    public function __construct(Layout $layout) {
-        $this->layout = $layout;
+    public function __construct(Layout $layout)
+    {
+        $this->_layout = $layout;
     }
 
     /**
-     * Controlleur de Account.
-     * TO DO!!!!
+     * Contrôleur de la page 'Compte'
+     *
      * @return void
      */
-    public function show(): void {
+    public function show(): void
+    {
 
         if (!isset($_SESSION['identifier'])) {
             header('Location: /intramu');
@@ -41,8 +79,8 @@ class Account {
         $cssFilePath = '_assets/styles/account.css';
         $jsFilePath = '_assets/scripts/account.js';
 
-        $this->layout->renderTop($title, $cssFilePath);
+        $this->_layout->renderTop($title, $cssFilePath);
         $view->showView();
-        $this->layout->renderBottom($jsFilePath);
+        $this->_layout->renderBottom($jsFilePath);
     }
 }
