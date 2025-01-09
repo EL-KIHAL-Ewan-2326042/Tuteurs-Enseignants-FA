9b6e9f36b379e0de91e56b2139c2efc7894403e6
Titouan <titouan.alvares@etu-univ-amu.fr>
2025-01-09 09:27:28 +0100

    chores: linter controller AboutUs

--- modules/Controllers/Aboutus.php
+++ modules/Controllers/Aboutus.php
@@ -1,32 +1,72 @@
 <?php
+/**
+ * Fichier contenant le contrôleur de la page 'A propos'
+ *
+ * PHP version 8.3
+ *
+ * @category Controller
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
+
 namespace Blog\Controllers;
 
 use Blog\Views\layout\Layout;
 
-class Aboutus {
+/**
+ * Classe gérant les échanges de données entre
+ * le modèle et la vue de la page 'A propos'
+ *
+ * PHP version 8.3
+ *
+ * @category Controller
+ * @package  TutorMap/modules/Controllers
+ *
+ * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
+ * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
+ * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
+ * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
+ *
+ * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
+ * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
+ */
+class Aboutus
+{
 
-    private Layout $layout;
+    private Layout $_layout;
 
     /**
-     * Constructeur de la classe Aboutus (controller)
+     * Initialise les attributs passés en paramètre
+     *
      * @param Layout $layout Instance de la classe Layout
+     *                       servant de vue pour la mise en page
      */
-    public function __construct(Layout $layout) {
-        $this->layout = $layout;
+    public function __construct(Layout $layout)
+    {
+        $this->_layout = $layout;
     }
 
     /**
-     * Controlleur de la page a propos
+     * Contrôleur de la page 'A propos'
+     *
      * @return void
      */
-    public function show(): void {
+    public function show(): void
+    {
         $title = "A Propos";
         $cssFilePath = '';
         $jsFilePath = '';
         $view = new \Blog\Views\aboutus\Aboutus();
 
-        $this->layout->renderTop($title, $cssFilePath);
+        $this->_layout->renderTop($title, $cssFilePath);
         $view->showView();
-        $this->layout->renderBottom($jsFilePath);
+        $this->_layout->renderBottom($jsFilePath);
     }
 }
