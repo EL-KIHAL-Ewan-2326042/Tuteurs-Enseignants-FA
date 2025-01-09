509afab2c0a7198e5a014003779f12d21cd8f09e
Titouan <titouan.alvares@etu-univ-amu.fr>
2025-01-09 17:30:07 +0100

    chores: début linting

--- _assets/includes/exceptions/RouterException.php
+++ _assets/includes/exceptions/RouterException.php
@@ -3,12 +3,15 @@
 /**
  * Exception si aucune route n'est trouvée par le router
  */
-class RouterException extends Exception{
-    public function __construct($message, $code = 0, Exception $previous = null){
+class RouterException extends Exception
+{
+    public function __construct($message, $code = 0, Exception $previous = null)
+    {
         parent::__construct($message, $code, $previous);
     }
 
-    public function __toString(){
+    public function __toString()
+    {
         return __CLASS__ . ": [{$this->code}]: {$this->message}\n";
     }
 
