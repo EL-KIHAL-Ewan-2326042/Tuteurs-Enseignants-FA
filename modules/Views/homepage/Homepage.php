ccd39a42bd1358a598ba68803579d0612ca1adbb
Titouan <titouan.alvares@etu-univ-amu.fr>
2025-02-12 09:14:35 +0100

    chores: linter

--- modules/Views/homepage/Homepage.php
+++ modules/Views/homepage/Homepage.php
@@ -451,12 +451,12 @@ readonly class Homepage
                                                 '_', ' ',
                                                 $row["formation"]
                                             )
-                                        ?></td>
+                                            ?></td>
                                         <td><?php
                                             echo str_replace(
                                                 '_', ' ', $row["class_group"]
                                             )
-                                        ?></td>
+                                            ?></td>
                                         <td><?php
                                             echo $row['internshipTeacher'] > 0
                                                 ? $row['year'] : '❌';
@@ -466,17 +466,17 @@ readonly class Homepage
                                                 '_', ' ',
                                                 $row["company_name"]
                                             )
-                                        ?></td>
+                                            ?></td>
                                         <td><?php
                                             echo str_replace(
                                                 '_', ' ', $row["internship_subject"]
                                             )
-                                        ?></td>
+                                            ?></td>
                                         <td><?php
                                             echo str_replace(
                                                 '_', "'", $row['address']
                                             )
-                                        ?></td>
+                                            ?></td>
                                         <td>~<?php echo
                                             $row['duration'] . " minutes"
                                         ?></td>
