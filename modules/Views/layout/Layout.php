ad78d5faadce56573957c61cb953eb83805b71d3
Léo <146103418+SALENSON-Leo-2326093aa@users.noreply.github.com>
2025-05-20 16:17:51 +0200

    Layout + account UI UX refais

--- modules/Views/layout/Layout.php
+++ modules/Views/layout/Layout.php
@@ -18,22 +18,6 @@
 
 namespace Blog\Views\layout;
 
-/**
- * Classe gérant l'affichage de la mise en page
- *
- * PHP version 8.3
- *
- * @category View
- * @package  TutorMap/modules/Views/layout
- *
- * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
- * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
- * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
- * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
- *
- * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
- * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
- */
 class Layout
 {
 
@@ -50,116 +34,73 @@ class Layout
         ?>
         <!DOCTYPE html>
         <html lang="fr">
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1">
-                <meta name="description"
-                      content="Auteurs: Alvares Titouan, Avias Daphné,
-Kerbadou Islem, Pellet Casimir">
-                <title><?php echo $title; ?></title>
-                <link rel="icon" type="image/x-icon" href="/favicon.ico">
-                <link href="https://cdnjs.cloudflare.com/ajax/libs/
-materialize/1.0.0/css/materialize.min.css"
-                      rel="stylesheet">
-                <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
-                rel="stylesheet">
-                <link rel="stylesheet"
-                      href="https://cdn.jsdelivr.net/gh/openlayers/
-openlayers.github.io@master/en/v6.5.0/css/ol.css"
-                      type="text/css">
-                <link href="/_assets/styles/layout.css" rel="stylesheet">
-                <?php
-                echo '<link href="' . $cssFilePath . '" rel="stylesheet">';
-                ?>
-            </head>
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <meta name="description"
+                  content="TutorMap">
+            <title><?php echo $title; ?></title>
+            <link href="/_assets/font/AMUMonumentGrostesk.css" rel="stylesheet">
+            <link rel="icon" type="image/x-icon" href="/favicon.ico">
+            <link href="/_assets/styles/layout.css" rel="stylesheet">
+            <!-- Datatables -->
+            <link href="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.3.1/af-2.7.0/b-3.2.3/b-colvis-3.2.3/b-html5-3.2.3/b-print-3.2.3/cr-2.1.0/fc-5.0.4/fh-4.0.2/kt-2.12.1/r-3.0.4/rr-1.5.0/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.css" rel="stylesheet" integrity="sha384-md9BXRwcqjBMfArJ62lJid/IP6Dr4sEtRIML9sBJ/txsKg56kMVGl1tCkC7tv2rg" crossorigin="anonymous">
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js" integrity="sha384-VFQrHzqBh5qiJIU0uGU5CIW3+OWpdGGJM9LBnGbuIH2mkICcFZ7lPd/AAtI7SNf7" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js" integrity="sha384-/RlQG9uf0M2vcTw3CX7fbqgbj/h8wKxw7C3zu9/GxcBPRKOEcESxaxufwRXqzq6n" crossorigin="anonymous"></script>
+            <script src="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.3.1/af-2.7.0/b-3.2.3/b-colvis-3.2.3/b-html5-3.2.3/b-print-3.2.3/cr-2.1.0/fc-5.0.4/fh-4.0.2/kt-2.12.1/r-3.0.4/rr-1.5.0/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.js" integrity="sha384-nXnoPxa46dpbP4PvaPQZrVt6mo6Ux86MTtOV+l47cM9/JoPQjZmfYImsAWjlJgeG" crossorigin="anonymous"></script>
+            <?php
+            echo '<link href="' . $cssFilePath . '" rel="stylesheet">';
+            ?>
+        </head>
         <body>
-        <nav class="navbar">
-            <div class="nav-wrapper container">
-                <a href="#" data-target="mobile-demo" class="sidenav-trigger">
-                    <i class="material-icons">menu</i>
+            <header>
+                <a href="/homepage" class="logo">
+                    amU
                 </a>
-                <a href="/homepage" class="left brand-logo hide-on-med-and-down"
-                style="margin-left: 10px;">
-                    <img src="https://i.imgur.com/CcTBFfX.png"
-                    alt="Logo de AMU" height="55" width="130">
-                </a>
-                <ul class="center hide-on-med-and-down">
-                    <?php
-                    if (isset($_SESSION['identifier'])
-                        && isset($_SESSION['roles'])
-                        && is_array($_SESSION['roles'])
-                        && in_array('Enseignant', $_SESSION['roles'])
-                    ) {
-                        echo '<li><a href="/homepage">DEMANDE</a></li>';
-                    }
+                <nav>
+                    <ul>
+                        <?php
+                        if (isset($_SESSION['identifier'])
+                            && isset($_SESSION['roles'])
+                            && is_array($_SESSION['roles'])
+                            && in_array('Enseignant', $_SESSION['roles'])
+                        ) {
+                            echo '<li><a href="/homepage">DEMANDE</a></li>';
+                        }
                     if (isset($_SESSION['role_name'])
                         && $_SESSION['role_name'] === 'Admin_dep'
                     ) { ?>
-                        <li><a href="/tutoring">
-                            <?php echo 'STAGES'; ?>
-                            </a></li>
-                        <li><a href="/dashboard">
-                            <?php echo 'GESTION DES DONNEES'; ?>
-                        </a></li>
-                        <li><a href="/dispatcher">
-                            <?php echo 'REPARTITEUR'; ?>
-                        </a></li>
-                    <?php } ?>
-                    <li><a href="/intramu"><?php
-                    if (isset($_SESSION['identifier'])) {
-                        echo 'DECONNEXION';
-                    } else {
-                        echo 'INTRAMU';
-                    } ?>
-                        </a></li>
-                </ul>
-                <ul class="right">
+                        <li>
+                            <a href="/tutoring">
+                                tutoring
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/dashboard">
+                                dashboard
+                            </a>
+                        </li>
+                        <li>
+                            <a href="/dispatcher">
+                                dispatcher
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
+                <div>
                     <?php if (isset($_SESSION['identifier'])) { ?>
-                        <li class="user-identifier"><a href="/account">
-                            <?php echo $_SESSION['fullName']['teacher_firstname']
-                            . ' ' . $_SESSION['fullName']['teacher_name'] . ' (';
-                            $roles = '';
-                            foreach (array_unique($_SESSION['roles']) as $role) {
-                                $roles .= $role . ', ';
-                            }
-                            $roles = substr($roles, 0, -2);
-                            echo $roles . ')';
-                            ?> </a> </li>
+                        <a href="/account">
+                            <span><?php echo $_SESSION['fullName']['teacher_firstname']
+                                    . ' ' . $_SESSION['fullName']['teacher_name'] ?></span>
+                            <span><?php echo $_SESSION['identifier']?></span>
+                         </a>
                     <?php } ?>
-                </ul>
-            </div>
-        </nav>
-        <ul class="sidenav" id="mobile-demo">
-            <?php
-            if (isset($_SESSION['identifier'])
-                && isset($_SESSION['roles'])
-                && is_array($_SESSION['roles'])
-                && in_array('Enseignant', $_SESSION['roles'])
-            ) {
-                echo '<li><a href="/homepage">DEMANDE</a></li>';
-            }
-            if (isset($_SESSION['role_name'])
-                && $_SESSION['role_name'] === 'Admin_dep'
-            ) { ?>
-                <li><a href="/tutoring">
-                    <?php echo 'STAGES'; ?>
-                </a></li>
-                <li><a href="/dashboard">
-                    <?php echo 'GESTION DES DONNEES'; ?>
-                </a></li>
-                <li><a href="/dispatcher">
-                    <?php echo 'REPARTITEUR'; ?>
-                </a></li>
-            <?php } ?>
-            <li><a href="/intramu"><?php
-            if (isset($_SESSION['identifier'])) {
-                echo 'DECONNEXION';
-            } else {
-                echo 'INTRAMU';
-            } ?></a></li>
-        </ul>
+                </div>
+            </header>
         <?php
-    }
+    } }
 
     /**
      * Affiche le rendu du pied de page (footer)
@@ -171,38 +112,34 @@ openlayers.github.io@master/en/v6.5.0/css/ol.css"
     public function renderBottom(string $jsFilePath): void
     {
         ?>
-        <footer class="page-footer">
-            <div class="container">
-                <div class="row">
-                    <div class="col s6">
+        <footer>
+                    <div>
                         &copy; <?php echo date("Y"); ?> TutorMap
                     </div>
-                    <div class="col s6 right-align">
+                    <div >
                         <a href="/legal-notices">Mentions légales </a>-
                         <a href="/aboutus"> À propos</a>
                     </div>
-                </div>
-            </div>
         </footer>
         <script src="/_assets/scripts/layout.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/
 materialize/1.0.0/js/materialize.min.js"></script>
-        <?php
-        if ($jsFilePath) {
-            echo '<script src="' . $jsFilePath . '"></script>';
-        }
-        $currentUri = $_SERVER['REQUEST_URI'];
+    <?php
+    if ($jsFilePath) {
+        echo '<script src="' . $jsFilePath . '"></script>';
+    }
+    $currentUri = $_SERVER['REQUEST_URI'];
 
-        if ($currentUri === '/' || $currentUri === '/homepage'
-            || $currentUri === '/dispatcher'
-        ) {
-            echo '<script async defer
+    if ($currentUri === '/' || $currentUri === '/homepage'
+        || $currentUri === '/dispatcher'
+    ) {
+        echo '<script async defer
             src="https://cdn.jsdelivr.net/gh/openlayers/
 openlayers.github.io@master/en/v6.5.0/build/ol.js" 
             onload="initMap()">
           </script>';
-        }
-        ?>
+    }
+    ?>
         </body>
         </html>
         <?php
