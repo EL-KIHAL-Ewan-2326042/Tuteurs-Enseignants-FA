be8e8b8d062382ae57f3ca0584363a9d45ef7497
Léo <146103418+SALENSON-Leo-2326093aa@users.noreply.github.com>
2025-05-21 09:36:49 +0200

    feat: Page de connexion

--- modules/Views/intramu/Intramu.php
+++ modules/Views/intramu/Intramu.php
@@ -18,20 +18,6 @@
 
 namespace Blog\Views\intramu;
 
-/**
- * Classe gérant l'affichage de la page de connexion à l'Intramu
- *
- * @category View
- * @package  TutorMap/modules/Views/intramu
- *
- * @author Alvares Titouan <titouan.alvares@etu.univ-amu.fr>
- * @author Avias Daphné <daphne.avias@etu.univ-amu.fr>
- * @author Kerbadou Islem <islem.kerbadou@etu.univ-amu.fr>
- * @author Pellet Casimir <casimir.pellet@etu.univ-amu.fr>
- *
- * @license MIT License https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants/blob/main/LICENSE
- * @link    https://github.com/AVIAS-Daphne-2326010/Tuteurs-Enseignants
- */
 class Intramu
 {
 
@@ -53,41 +39,55 @@ class Intramu
     {
         ?>
         <main>
-            <div class="card-panel white z-depth-3">
-                <div class="row">
-                    <form class="col s12" action="./intramu"
-                    method="POST" id="formAssociate">
-                        <div class="row">
-                            <h1 class="indigo-text center-align">
-                                Aix-Marseille Université - Service d'authentification
-                            </h1>
+            <section>
+                <img
+                        src="https://i.imgur.com/AqAvrsS.png"
+                        alt="Image de l'université"
+                        decoding="async"
+                        fetchpriority="low"
+                        width="800"
+                        height="600"
+                >
+            </section>
+
+
+            <section class="form-section">
+                <article>
+                    <h1>Service d'authentification</h1>
+                    <p>Accédez au meilleur service pour les étudiants</p>
+                </article>
+                <form action="./intramu" method="POST" id="formAssociate" class="container">
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="identifier" name="identifier" type="text" class="validate" maxlength="20"
+                                   autocomplete="username" required>
+                            <label for="identifier">Identifiant</label>
                         </div>
-                        <div class="row center-align">
-                            <p class="red-text"><?php echo $this->errorMessage?></p>
+                    </div>
+
+                    <div class="row">
+                        <div class="input-field col s12">
+                            <input id="password" name="password" type="password" class="validate" maxlength="32"
+                                   autocomplete="current-password" required>
+                            <label for="password">Mot de passe</label>
                         </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="text" id="identifier" name="identifier"
-                                maxlength="20" class="validate">
-                                <label for="identifier">Identifiant</label>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input type="password" id="password" name="password"
-                                maxlength="32" class="validate">
-                                <label for="password">Mot de Passe</label>
-                            </div>
-                        </div>
-                        <div class="row center-align">
-                            <button class="btn waves-effect waves-light"
-                            type="submit" name="login" id="user">Connexion
-                                <i class="material-icons right">send</i>
+                    </div>
+
+                    <div class="row">
+                        <div class="col s12">
+                            <button class="btn waves-effect waves-light" type="submit" name="login" id="user">
+                                Connexion
                             </button>
                         </div>
-                    </form>
-                </div>
-            </div>
+                    </div>
+                </form>
+
+                <?php if (!empty($this->errorMessage)) : ?>
+                    <div role="alert" aria-live="assertive" class="error-message">
+                        <?= htmlspecialchars($this->errorMessage) ?>
+                    </div>
+                <?php endif; ?>
+            </section>
         </main>
         <?php
     }
--- modules/Views/layout/Layout.php
+++ modules/Views/layout/Layout.php
@@ -43,13 +43,43 @@ class Layout
             <link href="/_assets/font/AMUMonumentGrostesk.css" rel="stylesheet">
             <link rel="icon" type="image/x-icon" href="/favicon.ico">
             <link href="/_assets/styles/layout.css" rel="stylesheet">
-            <!-- Datatables -->
-            <link href="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.3.1/af-2.7.0/b-3.2.3/b-colvis-3.2.3/b-html5-3.2.3/b-print-3.2.3/cr-2.1.0/fc-5.0.4/fh-4.0.2/kt-2.12.1/r-3.0.4/rr-1.5.0/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.css" rel="stylesheet" integrity="sha384-md9BXRwcqjBMfArJ62lJid/IP6Dr4sEtRIML9sBJ/txsKg56kMVGl1tCkC7tv2rg" crossorigin="anonymous">
-            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js" integrity="sha384-VFQrHzqBh5qiJIU0uGU5CIW3+OWpdGGJM9LBnGbuIH2mkICcFZ7lPd/AAtI7SNf7" crossorigin="anonymous"></script>
-            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js" integrity="sha384-/RlQG9uf0M2vcTw3CX7fbqgbj/h8wKxw7C3zu9/GxcBPRKOEcESxaxufwRXqzq6n" crossorigin="anonymous"></script>
-            <script src="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.3.1/af-2.7.0/b-3.2.3/b-colvis-3.2.3/b-html5-3.2.3/b-print-3.2.3/cr-2.1.0/fc-5.0.4/fh-4.0.2/kt-2.12.1/r-3.0.4/rr-1.5.0/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.js" integrity="sha384-nXnoPxa46dpbP4PvaPQZrVt6mo6Ux86MTtOV+l47cM9/JoPQjZmfYImsAWjlJgeG" crossorigin="anonymous"></script>
+            <script src="https://code.jquery.com/jquery-3.7.1.min.js"
+                    integrity="sha384-TKffAe9yYtJqUGAz+kLRKT2F8j+cdKIBhKX5vq2HPJZGo0SExRACykjOZMWPz/mX"
+                    crossorigin="anonymous"
+                    defer></script>
+
+            <!-- Datatables -->
+            <!-- CSS DataTables -->
+            <link rel="preconnect" href="https://cdn.datatables.net" crossorigin>
+            <link rel="preload" as="style" href="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.3.1/af-2.7.0/b-3.2.3/b-colvis-3.2.3/b-html5-3.2.3/b-print-3.2.3/cr-2.1.0/fc-5.0.4/fh-4.0.2/kt-2.12.1/r-3.0.4/rr-1.5.0/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.css" crossorigin>
+            <link rel="stylesheet" href="https://cdn.datatables.net/v/dt/jszip-3.10.1/dt-2.3.1/af-2.7.0/b-3.2.3/b-colvis-3.2.3/b-html5-3.2.3/b-print-3.2.3/cr-2.1.0/fc-5.0.4/fh-4.0.2/kt-2.12.1/r-3.0.4/rr-1.5.0/sp-2.3.3/sl-3.0.0/sr-1.4.1/datatables.min.css"
+                  integrity="sha384-md9BXRwcqjBMfArJ62lJid/IP6Dr4sEtRIML9sBJ/txsKg56kMVGl1tCkC7tv2rg"
+                  crossorigin="anonymous"
+                  media="print" onload="this.media='all'">
+
+            <!-- Material Icons -->
+            <link rel="preconnect" href="https://fonts.googleapis.com" crossorigin>
+            <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" media="print" onload="this.media='all'">
+
+            <!-- JS libraries  -->
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/pdfmake.min.js"
+                    integrity="sha384-VFQrHzqBh5qiJIU0uGU5CIW3+OWpdGGJM9LBnGbuIH2mkICcFZ7lPd/AAtI7SNf7"
+                    crossorigin="anonymous"
+                    defer></script>
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/pdfmake/0.2.7/vfs_fonts.js"
+                    integrity="sha384-/RlQG9uf0M2vcTw3CX7fbqgbj/h8wKxw7C3zu9/GxcBPRKOEcESxaxufwRXqzq6n"
+                    crossorigin="anonymous"
+                    defer></script>
+
+            <!-- Exemple plus léger -->
+            <script src="https://cdn.datatables.net/v/dt/dt-2.3.1/datatables.min.js"
+                    integrity="..."
+                    crossorigin="anonymous"
+                    defer></script>
+
+
             <?php
             echo '<link href="' . $cssFilePath . '" rel="stylesheet">';
             ?>
@@ -89,7 +119,7 @@ class Layout
                 </div>
             </header>
         <?php
-    } 
+    }
 
     /**
      * Affiche le rendu du pied de page (footer)
